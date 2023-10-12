7e50c08627ca3fe81bc0bf7ab9c3a4ec7bf6d73c
ClemPech <clement.pecheux@gmail.com>
2023-10-12 13:36:56 +0200

    Creation d'une fonction qui lit un csv

--- data_reader.py
+++ data_reader.py
@@ -1,4 +1,18 @@
 import pandas as pd
+
 nom_csv = 'nom.csv'
-data = pd.read_csv(nom_csv)
-print (data)
+
+def read_data (nom_csv : str) -> pd.DataFrame:
+    """
+    La fonction lit un tableau csv et renvoi les données dans un data frame
+    
+    INPUT : 
+        - nom_csv : string du nom du fichier
+    
+    OUTPUT :
+        - data frame du fichier
+    """
+    data = pd.read_csv(nom_csv)
+    return (data)
+
+print (nom_csv)
