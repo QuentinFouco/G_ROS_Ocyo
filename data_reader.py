c6e67e7022ff8295834b06b50aadb3c365527186
ClemPech <clement.pecheux@gmail.com>
2023-10-22 13:02:19 +0200

    gen dict_fourn dict_camion

--- data_reader.py
+++ data_reader.py
@@ -19,19 +19,69 @@ def read_data (list_csv : list) -> pd.DataFrame:
     return (data)
 
 def affretement () -> dict:
+    """_summary_
+
+    Returns:
+        dict: Dictionnaire des couts d'affretement
+            dict : ('<depart>' : dict ('<arrivée>' : int(<cout>)))
+    """
     data = pd.read_csv('Affretement.csv')
     dict_affretement = {}
     for i, nom1 in enumerate(data.Sites):
         dict_longueurs = {}
         for j, nom2 in enumerate(data.Sites):
-            print(data[nom1][j])
+            # print(data[nom1][j])
             dict_longueurs[nom2] = data[nom1][j]
         dict_affretement[nom1] = dict_longueurs    
     return (dict_affretement)
 data = read_data(list_csv)
 
-print (data.Sites)
-print (data.Chaises)
+# print (data.Sites)
+# print (data.Chaises)
 dict_affretement = affretement()
-print (dict_affretement)
-print (dict_affretement.keys())
+# print (dict_affretement)
+# print (dict_affretement.keys())
+
+
+def gen_dict_fourn (dict_affretement : dict) -> dict:
+    """Génere le dictionnaire des fournisseurs à partir du tableau
+
+    Args:
+        dict_affretement (dict): Dictionnaire des couts d'affretement
+            dict : ('<depart>' : dict ('<arrivée>' : int(<cout>)))
+        
+    Returns:
+        dict: ('<produit>' : list['<ville>','<ville>'])
+    """
+    villes = dict_affretement.keys()
+    df_fourn = pd.read_csv('Fournisseur.csv')
+    dict_fournisseur = {}
+    matieres = ['Tissus', 'Mousse', 'Plastique', 'Tube']
+    for matiere in matieres:
+        fourn = []
+        for i, ville in enumerate(villes):
+            if df_fourn[matiere][i]==1:
+                fourn.append(ville)
+        print(matiere, fourn)
+        dict_fournisseur[matiere]=fourn
+    return(dict_fournisseur)
+        
+
+    
+gen_dict_fourn(dict_affretement)
+
+def gen_camion_cap() -> dict:
+    """Génere le dictionnaire des capacités des camions à partir du tableau
+
+    Returns:
+        dict ('<Produit>':int(capacité))
+    """
+    data = pd.read_csv('Camion.csv')
+    dict_affretement = {}
+    for i, Produit in enumerate(data.Produits):
+        dict_affretement[Produit]= data.Unit_camion[i]
+    return dict_affretement
+
+dict_camion = gen_camion_cap()
+# for prod in dict_camion.keys():
+#     print (prod, dict_camion[prod])
