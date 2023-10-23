6a8c3ebacf30d6df26b2f4decfb4b3fc79f4fca3
ClemPech <clement.pecheux@gmail.com>
2023-10-23 13:59:21 +0200

    def f du calcul du minimaux pour les usines finale

--- pfut.py
+++ pfut.py
@@ -70,26 +70,66 @@ def min_fourn_usine_i (dict_usine_i : dict, dict_fourn : dict, dict_affretement 
             dict : ('<matière>' : int(<capacité>))
 
     Returns:
-        dict: ('<usine>' : dict ('<matière>' : tuple('<fournisseur>', <cout>)))
+        dict: ('<usine>' : dict ('<matière>' : tuple('<fournisseur>', <cout>)
+                                'totalCost' : int(Somme des couts)))
     """
     dict_usine_i_cost = {}
     for usine in dict_usine_i.keys():
         if dict_usine_i[usine]>0:
             dict_cost_mat = {}
+            totalCost = 0
             for matiere in ['Tissus', 'Mousse', 'Plastique']:
                 aff_cost = []
                 for fourn in dict_fourn[matiere]:
                     aff_cost.append(2*dict_affretement[usine][fourn]/dict_camion[matiere]) #On multiplie par 2 car pour chaque chaise on a besoin de 2 fois chaque ressource
                 id_min = np.argmin(aff_cost)
                 dict_cost_mat[matiere] = (dict_fourn[matiere][id_min], aff_cost[id_min])
+                totalCost = totalCost + aff_cost[id_min]
+            dict_cost_mat['totalCost']= totalCost
             dict_usine_i_cost[usine] = dict_cost_mat
     return(dict_usine_i_cost)
 
-def min_fourn_usine_f (dict_usine_f : dict, dict_fourn : dict, dict_affretement : dict, dict_camion : dict) -> dict:
+def min_fourn_usine_f (dict_usine_f : dict, dict_usine_i_cost : dict, dict_fourn : dict, dict_affretement : dict, dict_camion : dict) -> dict:
+    """Calcul le chemin optimal entre les usines finales et leurs fournisseurs
+
+    Args:
+        dict_usine_f (dict): Dictionnaire des usines finales
+            dict : ('<ville>': int) 
+        dict_usine_i_cost (dict): Dictionnaire des usines intermédiaires avec leur fournisseur et leurs couts d'affretement
+            dict: ('<usine>' : dict ('<matière>' : tuple('<fournisseur>', <cout>)
+                            'totalCost' : int(Somme des couts)))
+        dict_fourn (dict): Dictionnaire des matières premières et de la localisation de leur fournisseur
+            dict : ('<produit>' : list['<ville>','<ville>'])
+        dict_affretement (dict): Dictionnaire des couts d'affretement
+            dict : ('<depart>' : dict ('<arrivée>' : int(<cout>)))
+        dict_camion (dict): Dictionnaire de la capacité des camions pour chaque matière
+            dict : ('<matière>' : int(<capacité>))
+
+    Returns:
+        dict: ('<usine>' : dict ('<matière>' : tuple('<fournisseur>', <cout>)
+                                'totalCost' : int(Somme des couts)))
+    """
     dict_usine_f_cost = {}
     for usine_f in dict_usine_f.keys():
         if dict_usine_f[usine_f]>0:
+            dict_cost_mat = {}
+            totalCost = 0
+            ### Recherche du meilleur fournisseur de tube
             aff_cost=[]
-
-    
+            matiere = 'Tube'
+            for fourn in dict_fourn[matiere]:
+                aff_cost.append(dict_affretement[usine_f][fourn]/dict_camion[matiere])
+            id_min = np.argmin(aff_cost)
+            dict_cost_mat[matiere] = (dict_fourn[matiere][id_min], aff_cost[id_min])
+            totalCost = totalCost + aff_cost[id_min]
+            ### Recherche du meilleur fournisseur de dossier et assise
+            aff_cost=[]
+            for usine_i in dict_usine_i_cost:
+                aff_cost.append(usine_i['totalCost'])
+            id_min = np.argmin(aff_cost)
+            dict_cost_mat['Usine_i'] = dict_usine_i_cost.keys()[id_min]
+            totalCost = totalCost + aff_cost[id_min]
+            dict_cost_mat['totalCost'] = totalCost
+            dict_usine_f_cost[usine_f] = dict_cost_mat
     return(dict_usine_f_cost)
+
