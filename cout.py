a93f6d225eeaf3384a80cce1d71662273abbcc58
ClemPech <clement.pecheux@gmail.com>
2023-11-01 16:44:45 +0100

    Description des fonctions et ajout de commentaires

--- cout.py
+++ cout.py
@@ -11,6 +11,8 @@ dict_previsions = gen_previsions_vente()
 dict_amortissement = gen_amortissement()
 
 ### Set up d'une architecture
+# Ordre des villes : 
+#       ['Madrid','Marseille','Turin','Munich','Bruxelles']
 table_usines_f = [
     [0,0,0,0,1],
     [0,0,0,1,0],
@@ -42,14 +44,18 @@ table_usines_f = [
     [1,1,1,1,0],
     [1,1,1,1,1]
 ]
-
+## Creation du dictionnaire des usines finales
 dict_usines_f = gen_usines_from_list(table_usines_f[-1])
-
+## Création du dictionnaire des usines intermédiaires
 dict_usines_i = gen_usines_from_list(table_usines_f[-1])
 
-### Recherche des chemins les moins couteux
+
+##### Recherche des chemins les moins couteux
+
+## Chemins optimaux pour les usines intermédiaires
 dict_usines_i_cost = min_fourn_usine_i(dict_usines_i, dict_fourn, dict_affretement, dict_camion)
 
+# Affichage du dictionnaire de plus court chemin entre les usines intermédiaires et leurs fournisseurs
 print('Arborescence usine intermediaire :')
 for usine in dict_usines_i_cost.keys():
     dict_usine = dict_usines_i_cost[usine]
@@ -58,8 +64,10 @@ for usine in dict_usines_i_cost.keys():
         print('\t\t', matiere, dict_usine[matiere])
         
 
+## Chemins optimaux pour les usines finales
 dict_usines_f_cost = min_fourn_usine_f(dict_usines_f, dict_usines_i_cost, dict_fourn, dict_affretement, dict_camion)
 
+# Affichage du dictionnaire de plus court chemin entre les usines finales et leurs fournisseurs
 print('\nArborescence usine intermediaire :')
 for usine in dict_usines_f_cost.keys():
     dict_usine = dict_usines_f_cost[usine]
@@ -68,8 +76,10 @@ for usine in dict_usines_f_cost.keys():
         print('\t\t', matiere, dict_usine[matiere])
 
             
+## Chemins optimaux pour les points de ventes
 dict_vente_cost = min_fourn_vente(dict_usines_f_cost, dict_affretement, dict_camion, dict_previsions)
 
+# Affichage du dictionnaire de plus court chemin entre les points de ventes et leurs fournisseurs
 print('\nArborescence point de vente :')
 for point_vente in dict_vente_cost.keys():
     dict_point_vente = dict_vente_cost[point_vente]
@@ -77,31 +87,61 @@ for point_vente in dict_vente_cost.keys():
     for matiere in dict_point_vente.keys():
         print('\t\t', matiere, dict_point_vente[matiere])
 
-### Calcul du cout 
+##### Calcul du cout global 
+
+## Definition de fonctions utiles
 def cout_affretement(dict_vente_cost : dict, dict_previsions : dict) -> float:
+    """Calcule le cout lié à l'affretement des marchandises
+
+    Args:
+        dict_vente_cost (dict): Dictionnaire des couts unitaires d'affretement des chaises
+            dict: ('<ville>' : dict ('Usine_f' : '<fournisseur>'
+                                'totalCost' : float(Somme des couts)
+                                'Volume' : int(nb equivalants chaises produits)))
+        dict_previsions (dict):  Dictionnaire des prévisions de vente par point de vente
+            dict ('<Ville>':int(NbChaise))
+
+    Returns:
+        float: cout d'affretement total
+    """
     cout = 0
     for point_vente in dict_previsions.keys():
         cout += dict_previsions[point_vente]*dict_vente_cost[point_vente]['totalCost']
     return(cout)
 
 def cout_amortissement(dict_usines_f : dict, dict_usines_i : dict, dict_amortissement : dict) -> float:
+    """_summary_
+
+    Args:
+        dict_usines_f (dict): Dictionnaire des usines finales
+            dict : ('<ville>': int)
+        dict_usines_i (dict): Dictionnaire des usines intermédiaires
+            dict : ('<ville>': int) 
+        dict_amortissement (dict): dictionnaire des couts d'amortissement de chaque machine
+            dict ('<Machine>': int(cout))
+
+    Returns:
+        float: cout d'amortissement total
+    """
     cout_machine_usine_f = dict_amortissement['Perceuse'] + dict_amortissement['Plieuse']
     cout_machine_usine_i = dict_amortissement['Colleuse'] + dict_amortissement['Presse'] + dict_amortissement['MoulSieg'] + dict_amortissement['MoulDos'] + dict_amortissement['Decoupeuse']
     cout = len(dict_usines_f.keys())*cout_machine_usine_f + len(dict_usines_i.keys())*cout_machine_usine_i
     return(cout)
 
+## Calcul et affichage des couts
 cout_aff = cout_affretement(dict_vente_cost, dict_previsions)
 print('cout_aff = ',cout_aff)
 cout_amo = cout_amortissement(dict_usines_f, dict_usines_i, dict_amortissement)
 print('cout_amo = ',cout_amo)
 
+##### Cascade pour répartir des volumes de demande dans les usines finales et intermédiaires
 for point_vente in dict_vente_cost.keys():
     dict_usines_f_cost[dict_vente_cost[point_vente]['Usine_f']]['Volume'] += dict_vente_cost[point_vente]['Volume']
 
 for usine_f in dict_usines_f_cost.keys():
     dict_usines_i_cost[dict_usines_f_cost[usine_f]['Usine_i'][0]]['Volume'] += dict_usines_f_cost[usine_f]['Volume']
 
-
+##### Préparation a l'affichage du graph
 import networkx as nx
 import matplotlib.pyplot as plt
 dict_colors = {}
